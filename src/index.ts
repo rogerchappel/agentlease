9fba116cde56ef53e4dd0c9d2f179a84e9da7424
Roger Chappel <miscanalysis@gmail.com>
2026-05-27 06:36:44 +1000

    feat: export public library API

--- src/index.ts
+++ src/index.ts
@@ -11,3 +11,11 @@ export {
 } from "./types.js";
 
 export { LedgerError, UsageError } from "./errors.js";
+export { checkLedger } from "./check.js";
+export { addLease, createLease } from "./grant.js";
+export { defaultLedgerPath, emptyLedger, readLedger, resolveLedgerPath, writeLedger } from "./ledger.js";
+export { matchesLease } from "./match.js";
+export { isPathInside, normalizeLedgerPath } from "./paths.js";
+export { findLease, revokeLease } from "./revoke.js";
+export { isActive, leaseStatus, withStatus } from "./status.js";
+export { addTtl, isExpired, parseTtl } from "./time.js";
